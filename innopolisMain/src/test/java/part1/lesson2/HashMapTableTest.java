bf919695e52166a2f5e151f6da87611feeb5343a
vempari <Vempari_2@mail.ru>
2019-11-04 17:38:05 +0300

    Task3 - Реализация интерфейса, прикручивание джинериков, дописывание тестов.

--- innopolisMain/src/test/java/part1/lesson2/HashMapTableTest.java
+++ innopolisMain/src/test/java/part1/lesson2/HashMapTableTest.java
@@ -2,22 +2,24 @@ package part1.lesson2;
 
 import org.junit.Test;
 
+import java.util.*;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class HashMapTableTest {
     @Test
     public void whenPutMapThenSize1() {
-        HashMapTable a = new HashMapTable();
+        HashMapTable<Integer, String> a = new HashMapTable<>();
 
-        a.put(1, "Hello");
+        a.put(1, "Hello ");
         a.put(4, "Vasya");
         assertThat(a.size(), is(2));
     }
 
     @Test
-    public void whenPutInTheSamePlace() {
-        HashMapTable a = new HashMapTable();
+    public void whenPutInTheSamePlaceThenSize1() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
         a.put(1, "Hello");
         a.put(1, "aaaa");
         assertThat(a.size(), is(1));
@@ -25,7 +27,7 @@ public class HashMapTableTest {
 
     @Test
     public void whenPutMapThenContainsKey() {
-        HashMapTable a = new HashMapTable();
+        HashMapTable<Integer, String> a = new HashMapTable<>();
         a.put(1, "Hello");
         a.put(4, "Vasya");
         assertThat(a.containsKey(1), is(true));
@@ -33,34 +35,38 @@ public class HashMapTableTest {
 
     @Test
     public void whenPutMapThenContainsWrongKey() {
-        HashMapTable a = new HashMapTable();
+        HashMapTable<Integer, String> a = new HashMapTable<>();
         a.put(1, "Hello");
         a.put(4, "Vasya");
         assertThat(a.containsKey(5), is(false));
     }
 
+    @Test
+    public void whenPutMapThenContainsValue() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
+        a.put(1, "Hello");
+        a.put(4, "Vasya");
+        assertThat(a.containsValue("Hello"), is(true));
+    }
 
     @Test
-    public void whenPutThenRemoveMapThenSize() {
-        HashMapTable a = new HashMapTable();
-        a.put("1", "Hello");
-        a.remove("1");
+    public void whenMapIsEmptyThenTrue() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
+        assertThat(a.isEmpty(), is(true));
+    }
+
+    @Test
+    public void whenPutThenRemoveMapThenSize1() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
+        a.put(1, "Hello");
+        a.remove(1);
         a.put(1, "Vasya");
         assertThat(a.size(), is(1));
     }
 
     @Test
-    public void whenPutMapThenSetThenNewValue() {
-        HashMapTable a = new HashMapTable();
-        a.put(1, "Hello");
-        a.put(4, "Vasya");
-        a.set(1, "Jenya");
-        assertThat(a.getKey(1), is("Jenya"));
-    }
-
-    @Test
-    public void whenPutMoreThen16() {
-        HashMapTable a = new HashMapTable();
+    public void whenPutMoreThen16ThenSizeMoreThen16() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
         a.put(1, "Hello");
         a.put(4, "Vasya");
         a.put(2, "a");
@@ -69,12 +75,12 @@ public class HashMapTableTest {
         a.put(1234, "d");
         a.put(100, "e");
         a.put(63, "f");
-        a.put("89", "g");
+        a.put(89, "g");
         a.put(84, "h");
         a.put(222, "i");
         a.put(12, "j");
-        a.put("3333", "k");
-        a.put(new HashObject(2, "bbb"), "l");
+        a.put(1234567, "k");
+        a.put(2134567, "l");
         a.put(112, "m");
         a.put(4321, "n");
         a.put(8888, "o");
@@ -82,18 +88,89 @@ public class HashMapTableTest {
     }
 
     @Test
-    public void when100Things() {
-        HashMapTable a = new HashMapTable();
-        for(int i = 0; i < 100; i++) {
-            a.put(i, i+i);
+    public void whenPutAndClearThenSize0() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
+        a.put(1, "Hello");
+        a.put(4, "Vasya");
+        a.put(2, "a");
+        a.put(-99, "b");
+        a.put(400, "c");
+        a.put(1234, "d");
+        a.put(100, "e");
+        a.put(63, "f");
+        a.put(89, "g");
+        a.put(84, "h");
+        a.put(222, "i");
+        a.put(12, "j");
+        a.put(1234567, "k");
+        a.put(2134567, "l");
+        a.put(112, "m");
+        a.put(4321, "n");
+        a.put(8888, "o");
+        a.clear();
+        assertThat(a.size(), is(0));
+    }
+
+    @Test
+    public void whenPut100ThenSize100() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
+        for (int i = 0; i < 100; i++) {
+            a.put(i, "i+i");
         }
         assertThat(a.size(), is(100));
     }
 
     @Test
-    public void whenNullPutInside() {
-        HashMapTable a = new HashMapTable();
+    public void whenNullPutInsideThenException() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
         a.put(null, null);
         assertThat("Нельзя дабавлять null", is("Нельзя дабавлять null"));
+    }
+
+    @Test
+    public void WhenPutMapAndReturnSetOfKeys() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
+        Set<Integer> set;
+        a.put(1, "Hello");
+        a.put(4, "Vasya");
+        a.put(2, "a");
+        set = a.keySet();
+        assertThat(set.size(), is(3));
+    }
+
+    @Test
+    public void WhenPutMapAndReturnCollectionOfValue() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
+        Collection<String> col;
+        a.put(1, "Hello");
+        a.put(4, "Vasya");
+        a.put(2, "a");
+        col = a.values();
+        assertThat(col.contains("Vasya"), is(true));
+    }
+
+    @Test
+    public void WhenPutMapAndReturnSetOfObjectsKeyAndValue() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
+        Set<Map.Entry<Integer, String>> set;
+        a.put(1, "Hello");
+        a.put(4, "Vasya");
+        a.put(2, "a");
+        set = a.entrySet();
+        assertThat(set.size(), is(3));
+    }
+
+    @Test
+    public void WhenPutMapInsideMapWithPutAllThenSizeMap1PlusMap2() {
+        HashMapTable<Integer, String> a = new HashMapTable<>();
+        HashMapTable<Integer, String> a1 = new HashMapTable<>();
+        a.put(1, "Hello");
+        a.put(4, "Vasya");
+        a.put(2, "a");
+        a1.put(8, "Zuk");
+        a1.put(9, "Pauk");
+        a1.put(10, "Karakatiza");
+        a.putAll(a1);
+        assertThat(a.get(9), is("Pauk"));
     }
 }
